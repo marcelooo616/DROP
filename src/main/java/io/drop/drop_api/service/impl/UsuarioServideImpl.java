a5d0b5819e5a344dd7d3a62c523384ca5d91abf0
Marcelo Santos <marcelooo2014@hotmail.com>
2024-07-30 23:35:18 -0300

    "Subindo classe service"

---
+++ src/main/java/io/drop/drop_api/service/impl/UsuarioServideImpl.java
@@ -0,0 +1,10 @@
+package io.drop.drop_api.service.impl;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioServideImpl {
+}
